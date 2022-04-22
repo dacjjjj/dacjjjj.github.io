3598880c4256dcb73b3b4ff67e7d08115599b3d6
dom <100240196+dacjjjj@users.noreply.github.com>
2022-04-22 21:21:02 +0100

    added fish sound on mouseover

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active');
 })
 
-const dvd = document.querySelector('#dvd')
-const dvdbutton = document.querySelector('#dvdbutton')
 
-dvdbutton.addEventListener('click', function() {
-    dvd.classList.toggle('is-active');
-    dvdbutton.classList.toggle('is-active');
+const body = document.querySelector('.mainimagecontainer')
+
+body.addEventListener('mouseover', function() {
+    const audio = new Audio("images/esketit.mp3");
+    audio.play();
 })
