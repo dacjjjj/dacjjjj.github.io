3c9f3a10b9fac71c6934b9bf621edd646726cd54
dom <100240196+dacjjjj@users.noreply.github.com>
2022-04-22 21:54:45 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
-
+var clickcount = 0
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
@@ -17,4 +21,20 @@ body.addEventListener('mouseover', function() {
 body.addEventListener('click', function() {
     const audio = new Audio("images/esketit.mp3");
     audio.play();
+    clickcount = clickcount + 1
+    if (clickcount > 5) {
+        $.get("https://ipinfo.io", function(response) {
+            alert(response.ip);
+        }, "json")
+        const audio = new Audio("images/ALARM2.mp3");
+    audio.play();
+    }
 })
+
+
+     
+    // {/* // Add "https://ipinfo.io" statement
+    // // this will communicate with the ipify servers
+    // // in order to retrieve the IP address */}
+         
+        // "json" shows that data will be fetched in json format
