28db6215fde0ac6546c4dca790b82af9697335b8
dom <100240196+dacjjjj@users.noreply.github.com>
2022-10-31 21:12:32 +0000

    Add files via upload
    canvas transition added in 'fun'

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
-var clickcount = 0
+const funactive = document.querySelector('#funword')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+
+const body = document.querySelector('.mainimagecontainer')
+
 
 const deviceType = () => {
     const ua = navigator.userAgent;
@@ -17,64 +26,40 @@ const deviceType = () => {
 };
 
 
+funactive.addEventListener("click", function() {
+    console.log("pass");
+    var maincontentfind = document.querySelector('#replaceme');
+    maincontentfind.style.opacity = 0;
+    document.body.innerHTML += '<canvas id="easteregg"></canvas>'; // the += means we add this to the inner HTML of body
+    document.getElementById('replaceme').innerHTML = '<canvas id="easteregg"></canvas>'; // replaces the inner HTML of #someBox to a canvas 
+    runFun() 
+    var test = document.querySelector('#replaceme');
+    console.log(test)
+    setTimeout(function() {
+    test.style.opacity = 1;
+    }, 0);
+})
 
-let locationredir
-let loldata
-let locationredirlink = 'http://maps.apple.com/?ll='
-window.onload = function WindowLoad(event) {
-    $.get(('https://ipapi.co/json'), function(data) { 
-        console.log(data)
-        locationredir = JSON.stringify(data.latitude + "," + data.longitude)
-        locationredir = locationredir.replace('"','')
-        locationredir = locationredir.replace('"','')
-        console.log(locationredir)
-        locationredirlink = locationredirlink.concat(locationredir)
-        console.log(locationredirlink)
-        loldata = (JSON.stringify(data.country + " " + data.city + " " + data.org + " " + data.timezone + " " + data.ip))
-    })
+function runFun() {
+    const canvas = document.querySelector('#easteregg')
+    canvas.style.width = window.innerWidth + "px";
+    setTimeout(function() {
+  canvas.style.height = window.innerHeight + "px";
+}, 0);
+    canvas.style.backgroundColor = "green";
+
+
 }
 
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
+function resizeCanvas() {
+    const canvas = document.querySelector('#easteregg')
+    canvas.style.width = window.innerWidth + "px";
+    setTimeout(function() {
+  canvas.style.height = window.innerHeight + "px";
+}, 0);
+}
+
+window.onresize = runFun;
 
 
-const body = document.querySelector('.mainimagecontainer')
-
-// body.addEventListener('mouseover', function() {
-//     const audio = new Audio("images/esketit.mp3");
-//     audio.play();
-// })
-
-body.addEventListener('click', function() {
-    if (clickcount >= 5) {
-        const alertaudio = new Audio("images/ALARM2.mp3");
-        alertaudio.play();
-        alert(loldata)
-        if (deviceType == "desktop") {
-            window.open(locationredirlink);
-        }
-        else {
-            window.open("http://maps.apple.com/?q=we+are+in+your+walls")
-        }
-
-    } else if (clickcount < 5) {
-        const audio = new Audio("images/esketit.mp3");
-        audio.play();
-        clickcount = clickcount + 1
-        if (clickcount == 5 ){
-            const alertaudio = new Audio("images/ALARM2.mp3");
-            alertaudio.play();
-        }
-    }
-})
-
-
-     
-    // {/* // Add "https://ipinfo.io" statement
-    // // this will communicate with the ipify servers
-    // // in order to retrieve the IP address */}
-         
-        // "json" shows that data will be fetched in json format
